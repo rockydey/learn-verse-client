a7e5207df74b0b92b184cb5b80821e6236ef6ca5
Rocky Dey <coderrocky25@gmail.com>
2024-06-02 13:10:42 +0600

    update note successfully

--- src/pages/DashboardPages/ManageNotes/StudentNote.jsx
+++ src/pages/DashboardPages/ManageNotes/StudentNote.jsx
@@ -1,9 +1,37 @@
 import Swal from "sweetalert2";
 import useAxoisSecure from "../../../hooks/useAxoisSecure";
+import { useState } from "react";
+import { Modal } from "flowbite-react";
+import toast, { Toaster } from "react-hot-toast";
 
 const StudentNote = ({ note, refetch }) => {
   const axoisSecure = useAxoisSecure();
   const { title, _id, description } = note;
+  const [openModal, setOpenModal] = useState(false);
+  const [updateId, setUpdateId] = useState(null);
+
+  const handleUpdateNote = (event) => {
+    setOpenModal(false);
+    event.preventDefault();
+    const form = event.target;
+    const title = form.title.value;
+    const description = form.description.value;
+
+    const updateInfo = {
+      title,
+      description,
+    };
+
+    axoisSecure
+      .patch(`/student-notes/${updateId}`, updateInfo)
+      .then((res) => {
+        if (res.data.modifiedCount > 0) {
+          toast.success("Note updated successfully!");
+        }
+        refetch();
+      })
+      .catch((error) => toast.error(error.message));
+  };
 
   const handleDeleteNote = (id) => {
     Swal.fire({
@@ -41,7 +69,12 @@ const StudentNote = ({ note, refetch }) => {
         <p>{description}</p>
       </div>
       <div className='flex-none flex justify-between items-center'>
-        <button className='bg-color1 px-3 py-2 text-color4 font-semibold'>
+        <button
+          onClick={() => {
+            setUpdateId(_id);
+            setOpenModal(!openModal);
+          }}
+          className='bg-color1 px-3 py-2 text-color4 font-semibold'>
           Update Note
         </button>
         <button
@@ -50,6 +83,55 @@ const StudentNote = ({ note, refetch }) => {
           Delete Note
         </button>
       </div>
+      <Modal
+        show={openModal}
+        size='xl'
+        onClose={() => setOpenModal(!openModal)}
+        popup>
+        <Modal.Header className='bg-color5 text-color9 border-b-0' />
+        <Modal.Body className=' bg-color5  rounded-b'>
+          <div className=''>
+            <h5 className='text-2xl mb-5 text-center font-bold font-merriweather text-color1'>
+              Update Note
+            </h5>
+            <form
+              onSubmit={handleUpdateNote}
+              className='max-w-md mx-auto text-color9 rounded-xl  space-y-4'>
+              <div className='space-y-1'>
+                <label htmlFor='title' className='text-lg font-semibold '>
+                  Title:
+                </label>
+                <input
+                  id='title'
+                  type='text'
+                  className='w-full border-0 bg-color7 text-color5 rounded-md'
+                  defaultValue={title}
+                />
+              </div>
+              <div className='space-y-1'>
+                <label htmlFor='description' className='text-lg font-semibold '>
+                  Description:
+                </label>
+                <textarea
+                  rows={5}
+                  id='description'
+                  type='text'
+                  defaultValue={description}
+                  className='w-full border-0 bg-color7 text-color5 rounded-md'
+                />
+              </div>
+              <div className='text-center'>
+                <input
+                  type='submit'
+                  value='Update Note'
+                  className='bg-color1 px-4 py-3 font-semibold text-base text-color4 uppercase cursor-pointer'
+                />
+              </div>
+            </form>
+            <Toaster />
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
