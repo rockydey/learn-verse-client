55e83b680614d373f4ecc808a76fe33b73470928
Rocky Dey <coderrocky25@gmail.com>
2024-06-03 10:10:43 +0600

    show rejection feedback

--- src/pages/DashboardPages/ViewSessions/ViewSessions.jsx
+++ src/pages/DashboardPages/ViewSessions/ViewSessions.jsx
@@ -10,9 +10,13 @@ import {
   TableHeadCell,
   TableRow,
 } from "flowbite-react";
+import { useState } from "react";
+import { Modal } from "flowbite-react";
 
 const ViewSessions = () => {
   const { user } = useAuth();
+  const [feedback, setFeedback] = useState({});
+  const [openModal, setOpenModal] = useState(false);
   const axoisSecure = useAxoisSecure();
   const { data: sessions = [], refetch } = useQuery({
     queryKey: [user?.email, "sessions"],
@@ -50,7 +54,7 @@ const ViewSessions = () => {
                           : session.status === "reject"
                           ? "bg-color10"
                           : "bg-color11"
-                      } px-2 py-1 rounded capitalize text-color4 font-semibold`}>
+                      } px-2 py-1 rounded cursor-default capitalize text-color4 font-semibold`}>
                       {session.status}
                     </button>
                   </TableCell>
@@ -62,7 +66,12 @@ const ViewSessions = () => {
                         </button>
                       </TableCell>
                       <TableCell>
-                        <button className='px-2 bg-color5 py-1 rounded capitalize text-color4 font-semibold'>
+                        <button
+                          onClick={() => {
+                            setFeedback(session);
+                            setOpenModal(!openModal);
+                          }}
+                          className='px-2 bg-color5 py-1 rounded capitalize text-color4 font-semibold'>
                           Feedback
                         </button>
                       </TableCell>
@@ -73,6 +82,24 @@ const ViewSessions = () => {
             </TableBody>
           </Table>
         </div>
+        <Modal
+          show={openModal}
+          size='xl'
+          onClose={() => setOpenModal(!openModal)}
+          popup>
+          <Modal.Header className='bg-color9 text-color5 border-b-0' />
+          <Modal.Body className=' bg-color9  rounded-b'>
+            <div className='space-y-3 text-center'>
+              <h5 className='text-lg font-bold text-color5'>
+                Rejection Reason :{" "}
+                <span className='font-medium'>{feedback.rejection_reason}</span>
+              </h5>
+              <p className="font-semibold text-base text-color6">
+                Feedback : <span className="font-normal">{feedback.feedback}</span>
+              </p>
+            </div>
+          </Modal.Body>
+        </Modal>
       </div>
     </div>
   );
