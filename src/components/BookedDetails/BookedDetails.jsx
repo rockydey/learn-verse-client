0b4e9c6d39227aee1ba501d959909cc79dc64315
Rocky Dey <coderrocky25@gmail.com>
2024-06-06 23:00:34 +0600

    feedback added complete

--- src/components/BookedDetails/BookedDetails.jsx
+++ src/components/BookedDetails/BookedDetails.jsx
@@ -5,9 +5,16 @@ import useAxoisSecure from "../../hooks/useAxoisSecure";
 import { GiTeacher } from "react-icons/gi";
 import { TbTimeDuration45 } from "react-icons/tb";
 import { MdCategory } from "react-icons/md";
+import ReactStars from "react-rating-stars-component";
+import { render } from "react-dom";
+import { useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import toast, { Toaster } from "react-hot-toast";
 
 const BookedDetails = () => {
   const { id } = useParams();
+  const { user } = useAuth();
+  const [studentRating, setStudentRating] = useState(null);
   const axoisSecure = useAxoisSecure();
   const { data: bookedSession = [] } = useQuery({
     queryKey: ["booked-session"],
@@ -28,10 +35,33 @@ const BookedDetails = () => {
     session_category,
   } = bookedSession;
 
+  const ratingChanged = (newRating) => {
+    setStudentRating(newRating);
+  };
+
+  const handleAddFeedback = (event) => {
+    event.preventDefault();
+    const feedback = event.target.feedback.value;
+
+    const feedbackInfo = {
+      session_id,
+      rating: studentRating,
+      feedback,
+      user_email: user?.email,
+    };
+
+    axoisSecure.post("/feedbacks", feedbackInfo).then((res) => {
+      if (res.data.insertedId) {
+        toast.success("Thank you for feedback!");
+        event.target.reset();
+      }
+    });
+  };
+
   return (
-    <div>
+    <div className='md:max-w-2xl lg:max-w-screen-sm mx-auto px-2 md:px-4 lg:px-0'>
       <SectionTitle heading='Booking Details' subHeading='' />
-      <div className='mt-10 md:max-w-2xl lg:max-w-screen-sm mx-auto px-2 md:px-4 lg:px-0'>
+      <div className='mt-10'>
         <div className='bg-color9 text-color5 p-5 md:p-10 rounded-xl'>
           <h3 className='text-center  font-bold md:text-2xl'>
             {session_title}
@@ -53,14 +83,55 @@ const BookedDetails = () => {
           </div>
           <div className='mt-5 text-lg font-normal flex flex-col md:flex-row gap-3 md:gap-0 justify-evenly items-center'>
             <p>
-              <span className='font-medium'>Class Start :</span> {class_start}
+              <span className='font-medium'>Booking Date :</span> {booking_date}
             </p>
             <p>
-              <span className='font-medium'>Booking Date :</span> {booking_date}
+              <span className='font-medium'>Class Start :</span> {class_start}
             </p>
           </div>
         </div>
       </div>
+      <div className='mt-10 border-2 border-color6 p-10 rounded-xl'>
+        <h2 className='text-center font-merriweather text-2xl font-bold'>
+          Feedback Session
+        </h2>
+        <form onSubmit={handleAddFeedback} className='mt-5 space-y-2'>
+          <div className='flex items-center gap-4'>
+            <p className='text-lg font-semibold text-color5'>Rating :</p>
+            <ReactStars
+              count={5}
+              onChange={ratingChanged}
+              size={32}
+              isHalf={true}
+              emptyIcon={<i className='far fa-star'></i>}
+              halfIcon={<i className='fa fa-star-half-alt'></i>}
+              fullIcon={<i className='fa fa-star'></i>}
+              activeColor='#ffd700'
+            />
+          </div>
+          <div className=''>
+            <label
+              className='text-lg mb-2 inline-block font-semibold text-color5'
+              htmlFor='feedback'>
+              Feedback :
+            </label>
+            <textarea
+              className='w-full border-0 text-color5 rounded bg-color9'
+              rows={4}
+              name='feedback'
+              id='feedback'
+              placeholder='Your feedback'></textarea>
+          </div>
+          <div>
+            <input
+              type='submit'
+              value='Add Feedback'
+              className='bg-color1 px-3 py-2 font-semibold rounded cursor-pointer text-color4'
+            />
+          </div>
+        </form>
+      </div>
+      <Toaster />
     </div>
   );
 };
--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,14 +1,14 @@
 const SectionTitle = ({ heading, subHeading }) => {
   return (
-    <div className='text-center'>
+    <div className='text-center px-2 md:px-4 lg:px-0'>
       <div className='flex items-center justify-center'>
-        <div className='border-[3px] w-4 h-4 border-color1 rounded-full'></div>
-        <div className='border-b-[3px] border-color1 w-16'></div>
-        <h2 className='font-merriweather text-3xl px-3 font-bold text-color1'>
+        <div className='border-[3px] w-3 h-3 md:w-4 md:h-4 border-color1 rounded-full'></div>
+        <div className='border-b-[3px] border-color1 w-10 md:w-16'></div>
+        <h2 className='font-merriweather text-[22px] md:text-3xl px-3 font-bold text-color1'>
           {heading}
         </h2>
-        <div className='border-b-[3px] border-color1 w-16'></div>
-        <div className='border-[3px] w-4 h-4 border-color1 rounded-full'></div>
+        <div className='border-b-[3px] border-color1 w-10 md:w-16'></div>
+        <div className='border-[3px] w-3 h-3 md:w-4 md:h-4 border-color1 rounded-full'></div>
       </div>
       <h4 className='mt-4 lg:w-1/2 mx-auto text-lg text-color6 capitalize'>
         {subHeading}
